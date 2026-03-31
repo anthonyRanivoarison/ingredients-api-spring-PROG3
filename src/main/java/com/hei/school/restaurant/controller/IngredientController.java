982188b6a8d7e2929bc4a78b6009e436cd7d05f8
anthonyRanivoarison <antonitsiory@gmail.com>
2026-03-31 08:20:52 +0300

    update: add new endpoint for ingredient stock movemement

--- src/main/java/com/hei/school/restaurant/controller/IngredientController.java
+++ src/main/java/com/hei/school/restaurant/controller/IngredientController.java
@@ -1,13 +1,14 @@
 package com.hei.school.restaurant.controller;
 
 import com.hei.school.restaurant.entity.Ingredient;
+import com.hei.school.restaurant.entity.StockMovement;
+import com.hei.school.restaurant.entity.enums.Unit;
 import com.hei.school.restaurant.repository.IngredientRepository;
+import com.hei.school.restaurant.repository.StockMovementRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.Instant;
 import java.util.List;
 
 @RestController
@@ -15,9 +16,11 @@ import java.util.List;
 public class IngredientController {
 
     private final IngredientRepository ingredientRepository;
+    private final StockMovementRepository stockMovementRepository;
 
-    public IngredientController(IngredientRepository ingredientRepository) {
+    public IngredientController(IngredientRepository ingredientRepository, StockMovementRepository stockMovementRepository) {
         this.ingredientRepository = ingredientRepository;
+        this.stockMovementRepository = stockMovementRepository;
     }
 
     @GetMapping
@@ -33,5 +36,25 @@ public class IngredientController {
                     .body("Ingredient.id=" + id + " is not found");
         }
         return ResponseEntity.ok(ingredient);
+    }
+
+    @GetMapping("/{id}/stock")
+    public ResponseEntity<?> getStock(@PathVariable Integer id, @RequestParam(required = false) String at, @RequestParam(required = false) String unit) {
+        if (at == null || unit == null) {
+            return ResponseEntity.status(400)
+                    .body("Either query parameter `at` or `unit` is not provided.");
+        }
+
+        Ingredient ingredient = ingredientRepository.findById(id);
+        if (ingredient == null) {
+            return ResponseEntity.status(404)
+                    .body("Ingredient.id=" + id + " is not found");
+        }
+
+        Instant instant = Instant.parse(at);
+        Unit stockUnit = Unit.valueOf(unit.toUpperCase());
+
+        StockMovement stock = stockMovementRepository.getStockAt(id, instant, stockUnit);
+        return ResponseEntity.ok(stock);
     }
 }
